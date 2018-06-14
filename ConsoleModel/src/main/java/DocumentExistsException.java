ac974de7562f30d73e12b9c1d9cc9b04d611d54d
Student <dragondogpro@gmail.com>
2018-06-14 11:35:05 +0500

    Изменена структура проекта

--- ConsoleModel/src/main/java/DocumentExistsException.java
+++
@@ -1,2 +0,0 @@
-public class DocumentExistsException extends Throwable {
-}
--- ConsoleModel/src/main/java/ru/solomatnikov/exception/DocumentExistsException.java
+++
@@ -1,7 +0,0 @@
-package ru.solomatnikov.exception;
-
-public class DocumentExistsException extends Throwable {
-    public DocumentExistsException(String message) {
-        super(message);
-    }
-}
